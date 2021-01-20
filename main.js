513effa30e1968b45542865c74ad386749436f01
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2021-01-20 19:07:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-//https://teachablemachine.withgoogle.com/models/uk7mQtfgn/
+
+
+
+
+
+Webcam.set({
+    width:360,
+    height: 250,
+    image_format:'png',
+    png_quality:100
+   
+});
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function Take_snapshot(){
+ Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML="<img id='result' src='"+ data_uri+"'>";
+ });
+}
+
+
+console.log("ml5 version",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uk7mQtfgn/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("modalLoaded");
+}
+
